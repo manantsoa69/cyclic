b0e10861b8660940b60e9e27bc732dd8fa1be091
manantsoa69 <138488477+manantsoa69@users.noreply.github.com>
2023-08-08 21:34:46 +0200

    Add files via upload

--- routes/fbWebhookRoute.js
+++ routes/fbWebhookRoute.js
@@ -1,70 +1,85 @@
-// routes/fbWebhookRoute.js
 const express = require('express');
 const router = express.Router();
-const { saveSubscription } = require('../helper/saveSubscription');
 const { checkSubscription } = require('../helper/subscriptionHelper');
 const { sendMessage } = require('../helper/messengerApi');
 const { chatCompletion } = require('../helper/openaiApi');
 const { checkNumber } = require('./numberValidation');
+const axios = require('axios');
 
-// Handle POST requests for incoming messages
+// Function to save the response and prompt to Supabase
+async function saveResponseToSupabase(prompt, response) {
+  try {
+    const supabaseUrl = 'https://zqfylsnexoejgcmaxlsy.supabase.co';
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxZnlsc25leG9lamdjbWF4bHN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkxNjAxMzgsImV4cCI6MjAwNDczNjEzOH0.dlyQU6eqpm14uPceuxZWIWbqWjNUIw9S6YnpXrsqu1k';
+
+    const { data, error } = await axios.post(
+      `${supabaseUrl}/rest/v1/chat_responses`,
+      { prompt, response },
+      {
+        headers: {
+          'apikey': supabaseKey,
+          'Content-Type': 'application/json',
+          'Prefer': 'return=representation',
+        },
+      }
+    );
+
+    if (error) {
+      console.error('Error saving to Supabase:', error.message);
+    } else {
+      console.log('Response saved to Supabase:', data);
+    }
+  } catch (error) {
+    console.error('Error occurred while saving to Supabase:', error.message);
+  }
+}
 router.post('/', async (req, res) => {
   try {
     const { entry } = req.body;
-    const { sender: { id: senderId }, message: { text: query } } = entry[0].messaging[0];
-    console.log(`${senderId}`);
+    if (entry && entry.length > 0 && entry[0].messaging && entry[0].messaging.length > 0) {
+      const { sender: { id: fbid }, message } = entry[0].messaging[0];
+      if (message && message.text) {
+        let { text: query } = message;
+        console.log(`${fbid}`);
+        // Check if the message is a number
+        if (/^\d+$/.test(query)) {
+          const numberValidationResult = await checkNumber(query, fbid);
+          await sendMessage(fbid, numberValidationResult);
+          console.log('Number message sent:', numberValidationResult);
+          return res.sendStatus(200);
+        }
 
-    // Check if the message is a number
-    if (/^\d+$/.test(query)) {
-      const numberValidationResult = await checkNumber(query, senderId);
-      await sendMessage(senderId, numberValidationResult);
-      console.log('Number message sent:', numberValidationResult);
-      return res.sendStatus(200);
-    }
+        const { subscriptionStatus } = await checkSubscription(fbid);
+        if (subscriptionStatus === 'A') {
+          console.time('NumberCheck');
+          // Check if the message contains a number greater than 5
+          const numbersGreaterThan5 = query.match(/\d+/g)?.filter((num) => Number(num) > 5);
+          if (numbersGreaterThan5?.length > 0) {
+            // Modify the prompt to replace the numbers greater than 5 with 3
+            numbersGreaterThan5.forEach((num) => {
+              query = query.replace(num, '3');
+            });
+          }
+          console.timeEnd('NumberCheck');
+          console.time('chat');
+          // Call the chatCompletion function to get the response
+          const result = await chatCompletion(query, fbid);
 
-    const { subscriptionStatus, expireDate } = await checkSubscription(senderId);
-    if (subscriptionStatus === 'No subscription') {
-      const newSubscriptionStatus = '10M';
-      const saved = await saveSubscription(senderId, newSubscriptionStatus);
-
-      if (saved) {
-        console.log('Saved successfully.');
-        await sendMessage(
-          senderId,
-          `Félicitations ! 🎉 Vous avez remporté un abonnement gratuit de 10 minutes pour découvrir notre chatbot, Win.
+          // Save the prompt and response to Supabase
+          saveResponseToSupabase(query, result.response);
           
-   Profitez de cette expérience unique et laissez-moi répondre à vos questions et vous offrir une assistance personnalisée.😉`
-        );
+          // Send the response to the user
+          await sendMessage(fbid, result.response);
+          console.timeEnd('chat');
+          console.log('OpenAI chat completion response sent.');
+        }
       } else {
-        console.log('Failed to save.');
-        await sendMessage(
-          senderId,
-          'Désolé, une erreur s\'est produite lors du traitement de votre abonnement. Veuillez réessayer ultérieurement.'
-        );
+        // If the message or message.text is undefined, send an automatic reply to the user
+        const errorMessage = `
+        Bonjour ! 👋
+        C'est un plaisir de vous rencontrer, je suis votre assistant virtuel, prêt à vous fournir une assistance efficace et professionnelle. N'hésitez pas à me poser vos questions, je suis là pour vous accompagner en toute circonstance. 😊.`;
+        sendMessage(fbid, errorMessage);
       }
-    } else if (subscriptionStatus === 'E') {
-      await sendMessage(
-        senderId,
-        `
-        📢 Votre abonnement a expiré. Afin de continuer à bénéficier des services de notre chatbot, nous vous invitons à renouveler votre abonnement.
-
-        Détails du renouvellement :
-        Prix : 9900 ariary 💰
-        Durée : 1 mois (24h/24) ⏰
-
-        Moyens de paiement acceptés :
-        Mvola : 0330540967
-        Airtel Money : 0332044955
-        Orange Money : 0323232224
-        (Tous les comptes sont au nom de RAZAFIMANANTSOA Jean Marc.)
-
-        Une fois le paiement effectué, veuillez nous fournir votre numéro (10 chiffres) pour la vérification.📲`
-      );
-      console.log('Expired subscription.');
-    } else {
-      const result = await chatCompletion(query, senderId);
-      await sendMessage(senderId, result.response);
-      console.log('Message sent successfully.');
     }
   } catch (error) {
     console.error('Error occurred:', error);
@@ -72,7 +87,6 @@ router.post('/', async (req, res) => {
 
   res.sendStatus(200);
 });
-
 
 // Handle GET requests for verification
 router.get('/', (req, res) => {
